e1bccfc0e5670ea20681e31adc2ad9ff0c84714f
笠原未来 <fum_o3o7@ezweb.ne.jp>
2017-08-01 09:05:03 +0900

    Initial Commit

---
+++ kaomoji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kaomoji
+//
+//  Created by 笠原未来 on 2017/08/01.
+//  Copyright © 2017年 笠原未来. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
